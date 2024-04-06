5f1bb5e874109e12d5b12633ba8609ce5de4704c
foreman7684 <foreman7684@gmail.com>
2024-04-05 23:18:39 -0300

    Update streamlit_app.py
    Se toman las lineas 51 a 53 y se mueven adentro del ciclo FOR de la linea 33

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
             values ('""" + ingredients_string + """','""" +name_on_order+"""')"""
 
@@ -48,6 +49,4 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response)
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
